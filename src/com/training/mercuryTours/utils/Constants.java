2276265c96d899fe77c0375f1664e7dbbd000c0f
glvsuresh <glvsuresh@hotmail.com>
2016-10-08 13:46:47 -0400

    Selenium Grid

---
+++ src/com/training/mercuryTours/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.training.mercuryTours.utils;
+
+public class Constants {
+	
+	
+	public static String sDriverPath=System.getProperty("user.dir")+"/lib";
+	public static String sReportPath="E:/SeleniumTraining/";
+
+}
